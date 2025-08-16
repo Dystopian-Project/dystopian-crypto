e581b1984eb343535d714cb0af69fe0bdbe75d23
dcx7c5 <dcxdevelopment@protonmail.com>
2025-08-16 06:33:42 +0200

    Introduce `hosts` module with initial sync functions, JSON-based host database, and new `dystopian-hosts` script.

---
+++ lib/hosts.sh
@@ -0,0 +1,22 @@
+# shellcheck shell=sh
+# shellcheck disable=SC2001
+# shellcheck disable=SC2154
+# shellcheck disable=SC2181
+
+sync_host_db() {
+  echo
+}
+
+sync_ssl_db() {
+  echo
+}
+
+sync_gpg_db() {
+  echo
+}
+
+host_sync() {
+  host="$1"
+  all_hosts="${2:-false}"
+
+}
